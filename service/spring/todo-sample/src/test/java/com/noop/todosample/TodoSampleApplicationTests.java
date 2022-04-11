f87cb7c4fc75b49354baa9ac0945d13019f5aa04
apsamuel <aaron.psamuel@gmail.com>
2022-04-11 11:04:04 -0400

    feat: spring boot init

---
+++ service/spring/todo-sample/src/test/java/com/noop/todosample/TodoSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noop.todosample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
