11abded0a3ba6bced78d51314ae644756b7b8099
apsamuel <aaron.psamuel@gmail.com>
2022-04-12 12:41:21 -0400

    feat: todo controller (assumed JSON views)

---
+++ service/spring/todo-sample/src/main/java/com/noop/todosample/TodoController.java
@@ -0,0 +1,34 @@
+package com.noop.todosample;
+import java.util.ArrayList;
+// import java.util.concurrent.atomic.AtomicLong;
+// import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang3.RandomStringUtils;
+
+
+
+class TodoParams {
+    public String body;
+    public ArrayList<String> files;
+}
+
+@RestController
+public class TodoController {
+
+    private static final String id = RandomStringUtils.randomAlphanumeric(15);
+
+    @PostMapping("/todo")
+    public Todo create(
+        @RequestBody TodoParams params
+    ) {
+        System.out.println(params.toString());
+        return new Todo(
+            id,
+            params.body,
+            params.files
+        );
+    }
+}
