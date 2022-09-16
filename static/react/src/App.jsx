8cc483302c58334e1d4577fdf9c1d92d6e0b1c2e
Chloe Wintzer <arbitrarytech@users.noreply.github.com>
2022-09-16 13:31:00 -0700

    change page title for demo

--- static/react/src/App.jsx
+++ static/react/src/App.jsx
@@ -148,7 +148,7 @@ const App = () => {
               src={logo}
             />
           </a>
-          <span>Todo Application</span>
+          <span>Sample Todo</span>
           <button
             className='form-button'
             title='New Todo'
